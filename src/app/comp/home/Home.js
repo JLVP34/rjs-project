1315f4a45259d4872f8e5b6c9725d3441c87ac0d
Juan Luis Vázquez <jlvp.34@gmail.com>
2019-05-30 22:03:20 +0200

    [RJS-1.0]: LInt changes

--- src/app/comp/home/Home.js
+++ src/app/comp/home/Home.js
@@ -5,7 +5,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import { Jumbotron, Button } from 'react-bootstrap';
 
-import HomeConstants from './Home.const';
 import SearchForm from '../../../shared/comp/forms/search-form/SearchForm';
 
 const styles = theme => ({});
